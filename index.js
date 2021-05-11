7221559f10950dd8f4675e4784bcf61e90ed41b7
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-05-11 13:17:04 -0400

    just makes it subscribe to the topic that I am testing.

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ import {
 } from './src/redux-stuff/furnacesSlice';
 
 messaging()
-  .subscribeToTopic('matt-topic')
+  .subscribeToTopic('ssi-topic-121170fb-95bd-464e-a2ad-9026908799b6--057d6f7d-e36d-460a-80e9-c128c55a3a15')
   .then(() => {
     store.dispatch(addToPushLog('Subscribed to topic!'));
   });
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  store.dispatch(addToPushLog('Message handled in the background! ' + JSON.stringify(remoteMessage)));
+  store.dispatch(addToPushLog('Message handled in the background! ' + remoteMessage.notification.title + '\n' + remoteMessage.notification.body));
 });
 
 messaging().onMessage(async remoteMessage => {
-  store.dispatch(addToPushLog('A new FCM message arrived! ' +  JSON.stringify(remoteMessage)));
+  store.dispatch(addToPushLog('A new FCM message arrived! \n' + remoteMessage.notification.title + '\n' + remoteMessage.notification.body));
 });
 
 const Root = () => (
