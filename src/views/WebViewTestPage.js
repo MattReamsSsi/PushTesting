fdbf7d60c99b0ce295784904535020caf66ff832
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-05-13 18:38:42 -0400

    now the embedded react web view works.

--- src/views/WebViewTestPage.js
+++ src/views/WebViewTestPage.js
@@ -5,22 +5,22 @@ import { WebView } from 'react-native-webview';
 const WebViewTestPage = () => {
     return (
         //<WebView source={{ uri: "file:///android_asset/matt-try.html" }} />
-    //     <WebView
-    //     style={{flex: 1}}
-    //     originWhitelist={['*']}
-    //     source={{uri:'file:///android_asset/index.html'}}
-    //     style={{ marginTop: 20 }}
-    //     javaScriptEnabled={true}
-    //     domStorageEnabled={true}
-    //   />
-      <WebView
+        <WebView
         style={{flex: 1}}
         originWhitelist={['*']}
-        source={{uri:'http://192.168.1.80:8080/'}}
+        source={{uri:'file:///android_asset/index.html'}}
         style={{ marginTop: 20 }}
         javaScriptEnabled={true}
         domStorageEnabled={true}
       />
+    //   <WebView
+    //     style={{flex: 1}}
+    //     originWhitelist={['*']}
+    //     source={{uri:'http://192.168.1.80:8080/'}}
+    //     style={{ marginTop: 20 }}
+    //     javaScriptEnabled={true}
+    //     domStorageEnabled={true}
+    //   />
     );
   };
   
