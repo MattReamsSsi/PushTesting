0409fd5c806a797fee9409bf5848606ceae397a7
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-07-12 13:00:54 -0400

    trying out injecting javascript.

--- src/views/WebViewTestPage.js
+++ src/views/WebViewTestPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { renderToString } from 'react-dom/server';
 
@@ -8,31 +8,39 @@ const WebViewTestPage = () => {
   const domString = renderToString(<DomComponent/>);
   console.log("domString:" + domString);
 
+  // const myScript = `
+  //     window.globalIpAddress = 192.168.1.88;
+  //     true;
+  //   `;
+
     return (
         // <WebView source={{ uri: "file:///android_asset/matt-try.html" }} />
-        <WebView
-        style={{flex: 1}}
-        originWhitelist={['*']}
-        source={{uri:'file:///android_asset/index.html'}}
-        style={{ marginTop: 20 }}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        onMessage={event => {
-          alert(event.nativeEvent.data);
-        }}
-      />
-      // <WebView source={{ html: domString }}></WebView>
-      // <WebView
+      //   <WebView
       //   style={{flex: 1}}
       //   originWhitelist={['*']}
-      //   source={{uri:'http://192.168.1.73:3000/'}}
+      //   source={{uri:'file:///android_asset/index.html'}}
       //   style={{ marginTop: 20 }}
       //   javaScriptEnabled={true}
       //   domStorageEnabled={true}
       //   onMessage={event => {
-      //       alert(event.nativeEvent.data);
-      //     }}
+      //     alert(event.nativeEvent.data);
+      //   }}
       // />
+      // <WebView source={{ html: domString }}></WebView>
+      // <View>
+        <WebView
+        style={{flex: 1}}
+        originWhitelist={['*']}
+        source={{uri:'http://192.168.1.88:3000/'}}
+        style={{ marginTop: 20 }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        onMessage={event => {
+            alert(event.nativeEvent.data);
+          }}
+          injectedJavaScriptBeforeContentLoaded={`window.ipAddress = "192.168.1.88"`}
+      />
+      
     );
   };
 
