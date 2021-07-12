495da761391634cdd1ac100db1f78538a7da2797
MattReamsSsi <54852285+MattReamsSsi@users.noreply.github.com>
2021-07-12 10:40:57 -0400

    to build w/new 12.5 xcode.

--- src/views/WebViewTestPage.js
+++ src/views/WebViewTestPage.js
@@ -9,30 +9,30 @@ const WebViewTestPage = () => {
   console.log("domString:" + domString);
 
     return (
-        //<WebView source={{ uri: "file:///android_asset/matt-try.html" }} />
-      //   <WebView
-      //   style={{flex: 1}}
-      //   originWhitelist={['*']}
-      //   source={{uri:'file:///android_asset/index.html'}}
-      //   style={{ marginTop: 20 }}
-      //   javaScriptEnabled={true}
-      //   domStorageEnabled={true}
-      //   onMessage={event => {
-      //     alert(event.nativeEvent.data);
-      //   }}
-      // />
-      // <WebView source={{ html: domString }}></WebView>
-      <WebView
+        // <WebView source={{ uri: "file:///android_asset/matt-try.html" }} />
+        <WebView
         style={{flex: 1}}
         originWhitelist={['*']}
-        source={{uri:'http://192.168.1.73:3000/'}}
+        source={{uri:'file:///android_asset/index.html'}}
         style={{ marginTop: 20 }}
         javaScriptEnabled={true}
         domStorageEnabled={true}
         onMessage={event => {
-            alert(event.nativeEvent.data);
-          }}
+          alert(event.nativeEvent.data);
+        }}
       />
+      // <WebView source={{ html: domString }}></WebView>
+      // <WebView
+      //   style={{flex: 1}}
+      //   originWhitelist={['*']}
+      //   source={{uri:'http://192.168.1.73:3000/'}}
+      //   style={{ marginTop: 20 }}
+      //   javaScriptEnabled={true}
+      //   domStorageEnabled={true}
+      //   onMessage={event => {
+      //       alert(event.nativeEvent.data);
+      //     }}
+      // />
     );
   };
 
